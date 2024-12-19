b6b2a86e81c45a3064a7425a0d081858f0aa1893
Praween Soni <pramrajkumar1234@gmail.com>
2024-12-19 20:01:31 +0530

    Home page code

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <>
+      <section className="HomeSec">
+        <p>
+          I am a new developer is a professional who is just starting their
+          journey in the field of web development.I am responsible for
+          designing, coding, and modifying websites, from layout to function,
+          according to a client's specifications. They strive to create visually
+          appealing sites that feature user-friendly design and clear
+          navigation.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Home;
